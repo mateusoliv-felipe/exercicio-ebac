0cd48e2dc3e615dd8d51820e2422f96d719fb781
mateusoliv-felipe <mateusoliv,souza@gmail.com>
2022-07-12 11:51:33 +0000

    Os arquivos gasolina.png e gasolina.py foram modificados

--- gasolina.py
+++ gasolina.py
@@ -7,6 +7,9 @@ import seaborn as sns
 
 df = pd.read_csv('gasolina.csv')
 
-figura = sns.lineplot(x='dia', y='venda', data=df)
-figura.set(xlabel='Dia', ylabel='Preço', title='Variação do Preço da Gasolina')
+figura = sns.lineplot(x='dia', y='venda', data=df, legend=False, markers=True)
+
+figura.set(xlabel='Dia', ylabel='Valor', title='Variação do Preço da Gasolina!')
 figura.figure.savefig("gasolina.png")
+
+#como já havia colocado essas informações na versão anterior, fiz pequenas alteraçoes e adicionei este comentário
